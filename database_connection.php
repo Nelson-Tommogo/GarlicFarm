d10f4e9b8d70ea30118f1ac95f4dfa78a098c5b2
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-10 09:12:11 +0300

    Harvest HQ Web

---
+++ database_connection.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost"; // Database host
+$username = "root"; // Database username
+$password = "genius"; // Database password
+$database = "your_database"; // Database name
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Connection successful
+echo "Connected to MySQL database successfully.";
+
+// Close the connection
+$conn->close();
+?>
